d25fcf28f808f9b02be5cb2af0a1858ac8ad93c0
hatoo <hato2000@gmail.com>
2023-09-28 22:34:27 +0900

    remove

--- src/graph.rs
+++ src/graph.rs
@@ -2,63 +2,6 @@ use petgraph::graphmap::NodeTrait;
 use petgraph::prelude::DiGraphMap;
 use std::collections::HashSet;
 
-pub fn scc<N: Eq + NodeTrait, E>(graph: &DiGraphMap<N, E>) -> Vec<HashSet<N>> {
-    fn dfs<N: Eq + NodeTrait, E>(
-        graph: &DiGraphMap<N, E>,
-        used: &mut HashSet<N>,
-        visited: &mut Vec<N>,
-        v: N,
-    ) {
-        used.insert(v);
-        for n in graph.neighbors(v) {
-            if !used.contains(&n) {
-                dfs(graph, used, visited, n);
-            }
-        }
-        visited.push(v);
-    }
-
-    fn rdfs<N: Eq + NodeTrait, E>(
-        graph: &DiGraphMap<N, E>,
-        used: &mut HashSet<N>,
-        set: &mut HashSet<N>,
-        v: N,
-    ) {
-        used.insert(v);
-        set.insert(v);
-        for n in graph.neighbors_directed(v, petgraph::Direction::Incoming) {
-            if !used.contains(&n) {
-                rdfs(graph, used, set, n);
-            }
-        }
-    }
-
-    let mut used = HashSet::new();
-    let mut visited = Vec::new();
-
-    for v in graph.nodes() {
-        if !used.contains(&v) {
-            dfs(graph, &mut used, &mut visited, v);
-        }
-    }
-
-    used.clear();
-
-    visited
-        .iter()
-        .rev()
-        .flat_map(|&v| {
-            if !used.contains(&v) {
-                let mut set = HashSet::new();
-                rdfs(graph, &mut used, &mut set, v);
-                Some(set)
-            } else {
-                None
-            }
-        })
-        .collect()
-}
-
 pub fn scc_sub<N: Eq + NodeTrait, E>(
     graph: &DiGraphMap<N, E>,
     sub_vs: &HashSet<N>,
@@ -153,7 +96,7 @@ fn test_scc() {
     graph.add_edge(2, 3, ());
     graph.add_edge(3, 1, ());
 
-    let mut scc = scc(&graph);
+    let mut scc = scc_sub(&graph, &graph.nodes().collect());
     scc.sort_by_key(|s| s.len());
 
     assert_eq!(scc[0], HashSet::from_iter(vec![0]));
