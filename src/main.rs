787b260b2fad5ffe85ffbc82ad6de6ba7033a20b
hatoo <hato2000@gmail.com>
2023-09-29 22:07:34 +0900

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use brilt::{cfg::Cfg, restructure::StructureAnalysis};
+use brilt::{cfg::Cfg, restructure::StructureAnalysis, rvsdg::Rvsdg};
 
 fn main() {
     let program = bril_rs::load_program();
 
     let cfg = Cfg::new(&program.functions[0].instrs);
     let sa = StructureAnalysis::new(cfg);
+    let rvsdg = Rvsdg::new(
+        &program.functions[0]
+            .args
+            .iter()
+            .map(|a| a.name.clone())
+            .collect::<Vec<_>>(),
+        sa,
+    );
 
-    println!("{}", sa);
+    println!("{:#?}", rvsdg);
 }
